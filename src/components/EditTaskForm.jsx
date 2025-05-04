b69c539fec07b95738e2b8974a9cf89ccc32c6eb
Sandesh-Ghimire0 <sandeshghimire1000@gmail.com>
2025-05-04 19:59:25 +0545

    setpup EditTaskForm

--- src/components/EditTaskForm.jsx
+++ src/components/EditTaskForm.jsx
@@ -1,46 +1,29 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { addTask } from '../store/taskSlice'
-import { postTask } from '../api/api'
-import { closeTaskForm } from '../store/uiSlice';
 
-function TaskForm() {
-    const dispatch = useDispatch()
-
-    async function handleAddTask(formData){
-        const data = Object.fromEntries(formData.entries())
-        const res = await postTask(data)
-
-        if(res.status == 200){
-            dispatch(addTask(res.data.data)) // i need to push the data with _id property
-        }
-
-        dispatch(closeTaskForm())
-        
-    }
-
-
+function EditTaskForm({setShowEditTaskForm, task}) {
+    const { title, description, status, priority, dueDate } = task
+    const formattedDate =  dueDate &&  new Date(dueDate).toISOString().split("T")[0];
     return (
         <section className="p-6 bg-white">
-            <h2 className="text-2xl font-bold mb-6 text-gray-800">Create a New Task</h2>
-            <form action={handleAddTask} className="space-y-4">
+            <h2 className="text-2xl font-bold mb-6 text-gray-800">Update Task</h2>
+            <form className="space-y-4">
                 <div>
                     <label htmlFor="title" className="block text-sm font-medium text-gray-700">Title:</label>
-                    <input type="text" name="title" id="title" className="mt-1 w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                    <input type="text" defaultValue={title} name="title" id="title" className="mt-1 w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" />
                 </div>
 
                 <div>
                     <label htmlFor="description" className="block text-sm font-medium text-gray-700">Description:</label>
-                    <textarea name="description" id="description" rows="3" className="mt-1 w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"></textarea>
+                    <textarea defaultValue={description} name="description" id="description" rows="3" className="mt-1 w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"></textarea>
                 </div>
 
                 <div>
                     <label htmlFor="status" className="block text-sm font-medium text-gray-700">Status:</label>
                     <select name="status" id="status" className="mt-1 w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500">
-                        <option value="pending">Pending</option>
-                        <option value="in-progress">In-Progress</option>
-                        <option value="completed">Completed</option>
-                        <option value="delayed">Delayed</option>
+                        <option value="pending" defaultValue={"pending" === status?true:false}>Pending</option>
+                        <option value="in-progress" defaultValue={"in-progress" === status?true:false}>In-Progress</option>
+                        <option value="completed" defaultValue={"completed" === status?true:false}>Completed</option>
+                        <option value="delayed" defaultValue={"delayed" === status?true:false}>Delayed</option>
                     </select>
                 </div>
 
@@ -48,15 +31,15 @@ function TaskForm() {
                     <label className="block text-sm font-medium text-gray-700 mb-1">Priority:</label>
                     <div className="flex gap-4">
                         <label className="flex items-center gap-2">
-                            <input type="radio" name="priority" value="low" className="accent-blue-500" />
+                            <input type="radio" name="priority" value="low" className="accent-blue-500" defaultChecked={"low"===priority?true:false}/>
                             <span>Low</span>
                         </label>
                         <label className="flex items-center gap-2">
-                            <input type="radio" name="priority" value="medium" className="accent-blue-500" />
+                            <input type="radio" name="priority" value="medium" className="accent-blue-500" defaultChecked={"medium"===priority?true:false}/>
                             <span>Medium</span>
                         </label>
                         <label className="flex items-center gap-2">
-                            <input type="radio" name="priority" value="high" className="accent-blue-500" />
+                            <input type="radio" name="priority" value="high" className="accent-blue-500" defaultChecked={"high"===priority?true:false}/>
                             <span>High</span>
                         </label>
                     </div>
@@ -64,21 +47,20 @@ function TaskForm() {
 
                 <div>
                     <label htmlFor="due-date" className="block text-sm font-medium text-gray-700">Due Date:</label>
-                    <input type="date" name="dueDate" id="due-date" className="mt-1 w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                    <input type="date" defaultValue={formattedDate} name="dueDate" id="due-date" className="mt-1 w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" />
                 </div>
 
                 <div className='flex gap-10'>
                     <button 
                         type="submit" 
                         className="w-full mt-4 bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg transition duration-200"
-                        onClick={handleAddTask}
                     >
-                        Add Task
+                        Save
                     </button>
 
                     <div 
                         className="flex justify-center w-full mt-4 bg-gray-600 hover:bg-gray-700 text-white font-semibold py-2 px-4 rounded-lg transition duration-200"
-                        onClick={()=>dispatch(closeTaskForm())}
+                        onClick={()=>setShowEditTaskForm(prev => !prev)}
                     >
                         cancel
                     </div>
@@ -86,7 +68,7 @@ function TaskForm() {
 
             </form>
         </section>
-    )
+  )
 }
 
-export default TaskForm
+export default EditTaskForm
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,70 +1,98 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { MdDelete } from "react-icons/md";
 import { FaEdit } from "react-icons/fa";
 import { deleteTask } from '../api/api';
 import { useDispatch } from 'react-redux';
 import { removeTask } from '../store/taskSlice';
+import EditTaskForm from './EditTaskForm';
 
 function Task({task}) {
+    const [showEditTaskForm, setShowEditTaskForm] = useState(false)
+
     const { title, description, status, priority, dueDate } = task
     const dispatch = useDispatch()
+
+
     async function handleDeleteTask(_id){
-        console.log(_id)
         const res = await deleteTask(_id)
         if(res.status == 200){
             dispatch(removeTask(_id))
         }
-        console.log(res)
     }
 
 
     return (
-        <div className="bg-white shadow-md rounded-xl p-4 border border-gray-100 w-full max-w-md">
-            <div className="flex justify-between items-start mb-2">
-                <h3 className="text-lg font-semibold text-gray-800">{title}</h3>
-                <span
-                    className={`text-xs px-2 py-1 rounded-full font-medium ${
-                        status === 'completed'
-                            ? 'bg-green-100 text-green-700'
-                            : status === 'in-progress'
-                            ? 'bg-yellow-100 text-yellow-700'
-                            : 'bg-gray-100 text-gray-700'
-                    }`}
-                >
-                    {status}
-                </span>
-            </div>
-
-            <p className="text-sm text-gray-600 mb-3">{description || 'No description provided.'}</p>
-
-            <div className="flex justify-between text-sm text-gray-700">
-                <div>
-                    <span className="font-medium">Priority:</span>{' '}
+        <div> 
+            <div className="bg-white shadow-md rounded-xl p-4 border border-gray-100 w-full max-w-md">
+                <div className="flex justify-between items-start mb-2">
+                    <h3 className="text-lg font-semibold text-gray-800">{title}</h3>
                     <span
-                        className={`capitalize ${
-                            priority === 'high'
-                                ? 'text-red-600'
-                                : priority === 'medium'
-                                ? 'text-yellow-600'
-                                : 'text-green-600'
+                        className={`text-xs px-2 py-1 rounded-full font-medium ${
+                            status === 'completed'
+                                ? 'bg-green-100 text-green-700'
+                                : status === 'in-progress'
+                                ? 'bg-yellow-100 text-yellow-700'
+                                : 'bg-gray-100 text-gray-700'
                         }`}
                     >
-                        {priority}
+                        {status}
                     </span>
                 </div>
 
-                <div>
-                    <span className="font-medium">Due:</span>{' '}
-                    {dueDate ? new Date(dueDate).toLocaleDateString() : 'N/A'}
+                <p className="text-sm text-gray-600 mb-3">{description || 'No description provided.'}</p>
+
+                <div className="flex justify-between text-sm text-gray-700">
+                    <div>
+                        <span className="font-medium">Priority:</span>{' '}
+                        <span
+                            className={`capitalize ${
+                                priority === 'high'
+                                    ? 'text-red-600'
+                                    : priority === 'medium'
+                                    ? 'text-yellow-600'
+                                    : 'text-green-600'
+                            }`}
+                        >
+                            {priority}
+                        </span>
+                    </div>
+
+                    <div>
+                        <span className="font-medium">Due:</span>{' '}
+                        {dueDate ? new Date(dueDate).toLocaleDateString() : 'N/A'}
+                    </div>
                 </div>
+
+                <div className='flex justify-between items-center'>
+                    <div>
+                        <input type="checkbox" className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'/>
+                    </div>
+
+                    <div className='flex gap-3 mt-3'>
+                        <button 
+                            className='flex justify-center p-2 text-xl bg-red-500 text-white rounded-full cursor-pointer hover:bg-red-700'
+                            onClick={()=>handleDeleteTask(task._id)}
+                        ><MdDelete/></button>
+                        <button 
+                            className='flex justify-center  items-center p-2 text-xl bg-blue-500 text-white rounded-full cursor-pointer hover:bg-blue-700'
+                            onClick={()=>setShowEditTaskForm(prev => !prev)}
+                        ><FaEdit /></button>
+                    </div>
+                </div>
+
             </div>
 
-            <div className='flex justify-between mt-3'>
-                <button 
-                    className='flex justify-center p-2 text-xl bg-red-500 text-white rounded-full cursor-pointer hover:bg-red-700'
-                    onClick={()=>handleDeleteTask(task._id)}
-                ><MdDelete/></button>
-                <button className='flex justify-center  items-center p-2 text-xl bg-blue-500 text-white rounded-full cursor-pointer hover:bg-blue-700'><FaEdit /></button>
+
+            <div>
+                {
+                    (showEditTaskForm) && (
+                        <div className="fixed inset-0 backdrop-blur flex items-center justify-center z-50">
+                            <div className="bg-white rounded-2xl shadow-lg p-6 w-full max-w-xl">
+                                <EditTaskForm setShowEditTaskForm={setShowEditTaskForm} task={task}/>
+                            </div>
+                        </div>
+                    )
+                }
             </div>
         </div>
     )
